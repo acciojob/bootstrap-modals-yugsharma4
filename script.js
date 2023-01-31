910a005c0a28919acf2198276f9dd48f27565412
yugsharma4 <65899617+yugsharma4@users.noreply.github.com>
2023-01-31 17:24:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+var myModal = document.getElementById('open-modal')
+var myInput = document.getElementById('newton-modal')
+
+myModal.addEventListener('show', function () {
+  myInput.focus()
+	alert("clicked")
+})
